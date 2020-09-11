aa4b7af4fdc18a53a5e4fc9a691b62a19449cb2a
Willian Ferreira Peixoto <willian.peixoto@cwi.com.br>
2020-09-11 20:05:03 -0300

    Cria estrutura inicial para chamadas HTTP

--- src/app.js
+++ src/app.js
@@ -1,9 +1,71 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.post('/account', (req, res) => {//TODO cria conta com ID, name e balance
+  var msg = validaCamposNovaConta(req);
+  if(msg != "") {
+    return res.status(400).json(msg);
+  }
+  return res.status(200).json("Conta cadastrada com sucesso");
+});
+
+app.post('/moneyIncrease', (req, res) => {//TODO deposito - recebe id e valor
+  //var msg = validaCamposDeposito(req);
+  console.log("moneyIncrease");
+  console.log("id: " + req.body.id);
+  console.log("money: " + req.body.money);
+  var msg = "";
+  if(msg != "") {
+    return res.status(400).json(msg);
+  }
+  return res.status(200).json("Deposito feito com sucesso");
+});
+
+app.post('/moneyDecrease', (req, res) => {//TODO saque - recebe id e valor
+  //var msg = validaCamposSaque(req);
+  console.log("moneyDecrease");
+  console.log("id: " + req.body.id);
+  console.log("money: " + req.body.money);
+  var msg = "";
+  if(msg != "") {
+    return res.status(400).json(msg);
+  }
+  return res.status(200).json("Saque feito com sucesso");
+});
+
+app.get('/account', (req, res) => {
+  console.log(req.param("id"));
+  //validar conta não encontrada
+  return res.status(200).json("Seu saldo é xxx");
+});
+
+app.delete('/account', (req, res) => {
+  console.log(req.param("id"));
+  //excluir conta conta não encontrada
+  //validar conta não encontrada
+  return res.status(200).json("Conta excluída com sucesso");
+});
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
+
+
+function validaCamposNovaConta(req) {
+  if(req.body.name == undefined) {
+    return "Campo name não encontrado";
+  }
+  if(req.body.balance == undefined) {
+    return "Campo balance não encontrado";
+  }
+  if(req.body.name == null || req.body.name == "") {
+    return "Campo name é obrigatório";
+  }
+  if(req.body.balance == null) {
+    return 'Campo balance é obrigatório';
+  }
+  return "";
+}
